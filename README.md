a2325e29e120b405e38ddaf6d4e1a9bdb72ebc6e
EmreKayaoglu <53196188+EmreKayaoglu@users.noreply.github.com>
2021-05-29 04:54:11 -0700

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,39 @@
-# weconnect-problem
-Created with CodeSandbox
+# weconnect-test
+This is React Project for We-Conect Challenge.
+
+Implemented functions:
+- Create menu items according to the api response
+  API returns menu items list simply. It is developed using node express and deployed in heroku server.
+- Display contents of url inside internal frame.
+- Responsive style
+
+## Relation techniques:
+- Language: javascript
+- Framework: React.js
+- React-redux
+- react-bootstrap
+- react-dom
+
+## Online Testing
+1. Goto [codesandbox.io](https://codesandbox.io/s/github/EmreKayaoglu/weconnect-test)
+2. Click the icon for open new tab
+<img width="500" src="https://github.com/EmreKayaoglu/weconnect-test/blob/main/readme/1.jpg">
+3. Then you can see the online site
+<img width="500" src="https://github.com/EmreKayaoglu/weconnect-test/blob/main/readme/2.jpg">
+
+## Offline Testing
+1. Please download source code from git.
+2. Go to the folder and install npm packages
+```bash
+npm install
+```
+   Or
+```bash
+yarn
+```
+3. Run project
+```bash
+npm start
+```
+## License
+[MIT](https://choosealicense.com/licenses/mit/)
---
+++ src/App.js
@@ -0,0 +1,96 @@
+import React from "react";
+import "./styles.css";
+
+const App = () => {
+  const [input, setInput] = React.useState("");
+  const [output, setOutput] = React.useState("");
+
+  const parseParam = (str) => {
+    let ret = { arr: [], cnt: 0 };
+    for (let i = 0; i < str.length; i++) {
+      if (str.charAt(i) === "+") ret.arr.push(true);
+      else if (str.charAt(i) === "-") ret.arr.push(false);
+      else {
+        ret.cnt = parseInt(str.substr(i), 10);
+        break;
+      }
+    }
+    return ret;
+  };
+  const cakeFlip = (str) => {
+    let param = parseParam(str);
+    let flipCnt = 0;
+    let arr = param.arr;
+    let cnt = param.cnt;
+    while (true) {
+      let idx = arr.indexOf(false);
+      if (idx === -1 || idx + cnt > arr.length) break;
+      for (let i = 0; i < cnt; i++) {
+        arr[idx + i] = !arr[idx + i];
+      }
+      flipCnt++;
+    }
+    console.log(arr);
+    if (
+      arr.filter((a) => {
+        return !a;
+      }).length
+    ) {
+      console.log("Impossible");
+      return "Impossible";
+    } else {
+      console.log(`Flip Count: ${flipCnt}`);
+      return flipCnt;
+    }
+  };
+
+  const checkValidInput = (str) => {
+    const regex = /^\d+(([\+-]+)\d+)+/g;
+    const found = str.match(regex);
+    if (found.length == 0 || found[0] != str) {
+      return false;
+    }
+    return true;
+  };
+
+  const calculate = () => {
+    let str = input.replaceAll(" ", "");
+    let valid = checkValidInput(str);
+    if (!valid) {
+      // input is invalid
+      setOutput("Invalid Input");
+      return;
+    }
+
+    setOutput("");
+    const regex = /([\+-]+)\d+/g;
+    const cases = str.match(regex);
+    let o = "";
+    cases.forEach((element, i) => {
+      const res = cakeFlip(element);
+      o += "Case #" + (i + 1) + ": " + res + " ";
+      setOutput(o);
+    });
+  };
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h2>Pancake Problem</h2>
+      </header>
+      <div>
+        <textarea
+          className="testInput"
+          onChange={(e) => setInput(e.target.value)}
+          value={input}
+        />
+        <textarea className="testOutput" value={output} disabled />
+      </div>
+      <button className="action" onClick={() => calculate()}>
+        Start Flip
+      </button>
+    </div>
+  );
+};
+
+export default App;
